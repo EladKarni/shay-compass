01ad15253d8927c51a1150b1ce1515ce7342b3cb
Elad <elad.karni@gmail.com>
2025-11-18 15:22:50 -0500

    fix: update remotePatterns in next.config.mjs to replace images.unsplash.com with shay-development.netlify.app

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'shay-development.netlify.app',
       },
       {
         protocol: 'https',
